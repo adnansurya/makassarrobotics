9209e5cb26b237d72a586e280a80e0835b548b66
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-07-21 11:59:55 +0700

    launched

--- public/games/firebase.-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAcHklpkVYko5ZyuQXaeHhGxa8F3UzxDp0",
-    authDomain: "mksrobotics.firebaseapp.com",
-    databaseURL: "https://mksrobotics.firebaseio.com",
-    projectId: "mksrobotics",
-    storageBucket: "mksrobotics.appspot.com",
-    messagingSenderId: "236823766418",
-    appId: "1:236823766418:web:31de7229a960e361"
-};
